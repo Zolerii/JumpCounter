8b7d7319e773e1c41e4eb8eb189f47851e7ee0e4
Tyler Pentecost <tyler8806@gmail.com>
2021-03-07 22:05:34 -0600

    Update JumpCounter.lua
    I used Vim's auto indentation function.

--- JumpCounter/JumpCounter.lua
+++ JumpCounter/JumpCounter.lua
@@ -13,9 +13,9 @@ local frame = CreateFrame("frame")
 -- Write jumps to data broker
 frame:SetScript("OnUpdate", function(self, elap)
 	elapsed = elapsed + elap
-  if elapsed < UPDATE_PERIOD then
-    return
-  end
+	if elapsed < UPDATE_PERIOD then
+		return
+	end
 
 	elapsed = 0
 	local fps = GetFramerate()
@@ -24,45 +24,45 @@ end)
 
 -- Write data broker tool tip
 function dataObj:OnTooltipShow()
-  self:AddLine("Total Jumps")
+	self:AddLine("Total Jumps")
 end
 
 function dataObj:OnEnter()
-  GameTooltip:SetOwner(self, "ANCHOR_NONE")
-  GameTooltip:SetPoint("TOPLEFT", self, "BOTTOMLEFT")
-  GameTooltip:ClearLines()
-  dataObj.OnTooltipShow(GameTooltip)
-  GameTooltip:Show()
+	GameTooltip:SetOwner(self, "ANCHOR_NONE")
+	GameTooltip:SetPoint("TOPLEFT", self, "BOTTOMLEFT")
+	GameTooltip:ClearLines()
+	dataObj.OnTooltipShow(GameTooltip)
+	GameTooltip:Show()
 end
 
 function dataObj:OnLeave()
-  GameTooltip:Hide()
+	GameTooltip:Hide()
 end
 
 hooksecurefunc( "AscendStop", function()
-  if JumpCounter == nil then
-    JumpCounter = 0
-  else
-    JumpCounter = JumpCounter + 1
-    if JumpCounter == 10 or JumpCounter == 100 or JumpCounter == 1000 or mod(JumpCounter, 5000) == 0 then
-      -- Formatting taken from JumpsCount addon by boolbazaur https://www.curseforge.com/wow/addons/jumps-count
-      DEFAULT_CHAT_FRAME:AddMessage("|TInterface\\Icons\\INV_Gizmo_supersappercharge:32|t |cFFDAA520 Congratulations, It's your " .. JumpCounter .. "th Jump! |TInterface\\Icons\\INV_Gizmo_supersappercharge:32|t")
-    end
-  end
+	if JumpCounter == nil then
+		JumpCounter = 0
+	else
+		JumpCounter = JumpCounter + 1
+		if JumpCounter == 10 or JumpCounter == 100 or JumpCounter == 1000 or mod(JumpCounter, 5000) == 0 then
+			-- Formatting taken from JumpsCount addon by boolbazaur https://www.curseforge.com/wow/addons/jumps-count
+			DEFAULT_CHAT_FRAME:AddMessage("|TInterface\\Icons\\INV_Gizmo_supersappercharge:32|t |cFFDAA520 Congratulations, It's your " .. JumpCounter .. "th Jump! |TInterface\\Icons\\INV_Gizmo_supersappercharge:32|t")
+		end
+	end
 end );
 
 -- slash command
 for i, v in ipairs({"jc", "jumpcounter", "jumpcount"}) do
-  _G["SLASH_JUMPCOUNTER" .. i] = "/" .. v
+	_G["SLASH_JUMPCOUNTER" .. i] = "/" .. v
 end
 
 SlashCmdList.JUMPCOUNTER = function(msg)
-  if not msg or msg == "" then
-    if JumpCounter == nil then
-      JumpCounter = 0
-    end
-    DEFAULT_CHAT_FRAME:AddMessage("You have jumped " .. JumpCounter .. " times.");
-  elseif ( msg == "clear") then
-    JumpCounter = 0
-  end
+	if not msg or msg == "" then
+		if JumpCounter == nil then
+			JumpCounter = 0
+		end
+		DEFAULT_CHAT_FRAME:AddMessage("You have jumped " .. JumpCounter .. " times.");
+	elseif ( msg == "clear") then
+		JumpCounter = 0
+	end
 end
